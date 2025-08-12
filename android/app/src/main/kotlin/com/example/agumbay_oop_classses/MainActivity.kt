3e07ed7500af502fa900ecd9f7de77dd6e4ea390
francineleey-sudo <francineleey@gmail.com>
2025-08-12 21:18:18 +0800

    new

---
+++ android/app/src/main/kotlin/com/example/agumbay_oop_classses/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.agumbay_oop_classses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'class1.dart';
+
+void main() {
+  var class1 = Class1('Asliah Olama');
+  class1.describe();
+}
