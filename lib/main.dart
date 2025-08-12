36967d9ddd1f46c7f30865b7ae48bac9d7cf5897
francineleey-sudo <francineleey@gmail.com>
2025-08-12 21:35:06 +0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
-import 'class1.dart';
+import 'olama.dart';
+import 'tanda.dart';
+import 'agumbay.dart';
+import 'mark.dart';
 
 void main() {
-  var class1 = Class1('Asliah Olama');
-  class1.describe();
+  var olama = Olama('Aslia Olama', 'cat');
+  olama.describe();
+  var tanda = Tanda('Nor-aida Tanda', 'dog');
+  tanda.describe();
+  var agumbay = Agumbay('Karyll Agumbay', 'cat');
+  agumbay.describe();
+  var mark = Mark('Mark Lucenicio', 'cat');
+  mark.describe();
 }
